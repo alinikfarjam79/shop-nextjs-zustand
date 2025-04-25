7d4c2017c2e593ab1d5b458c4878d6dcdd991b23
alinikfarjam79 <alinikitest@gmail.com>
2025-04-25 23:31:22 +0330

    the footer was added

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,9 @@ export default function RootLayout({ children }) {
           <Header />
           {children}
           <ToastContainer position="top-right" rtl />
-
+          <div className="w-full flex items-center justify-center my-5">
+            <p className="text-xl">developed by <a href="https://github.com/alinikfarjam79/shop-nextjs-zustand" target="_blank" className="text-blue-600">AliNikfarjam</a></p>
+          </div>
         </main>
       </body>
     </html>
